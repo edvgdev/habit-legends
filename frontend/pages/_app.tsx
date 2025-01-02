0e2b4e0d50e6221cd41eb7888d005fe88a55d8ad
edvgdev <edvg.dev@gmail.com>
2025-01-02 17:56:41 +0700

    Created main admin page and admin options

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,11 +1,12 @@
 import SideBar from "@/components/side-bar";
 import "@/styles/globals.css";
+import { poppins } from "@/utils/fonts";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div
-      className="main-layout"
+      className={`${poppins.className} main-layout`}
     >
       <div className="side-bar-placeholder">
         <SideBar />
--- frontend/pages/admin.tsx
+++ frontend/pages/admin.tsx
@@ -1,9 +1,17 @@
+import AdminSection from '@/components/admin-section'
 import React from 'react'
 
 const Admin = () => {
     return (
-        <div>
-            Admin
+        <div className='admin'>
+            <div className='admin-container'>
+                <h1>Admin Page</h1>
+                <AdminSection title='Habits' description='View, add, edit, and delete Habits.' buttonText='Manage Habits' />
+                <AdminSection title='Categories' description='View, add, edit, and delete Categories.' buttonText='Manage Categories' />
+                <AdminSection title='Ranks' description='View, add, edit, and delete Ranks.' buttonText='Manage Ranks' />
+                <AdminSection title='Stats' description='View, add, edit, and delete Stats.' buttonText='Manage Stats' />
+                <AdminSection title='User Plans' description='View, add, edit, and delete User Plans.' buttonText='Manage User Plans' />
+            </div>
         </div>
     )
 }
